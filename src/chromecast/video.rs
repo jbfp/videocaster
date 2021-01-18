37115ea891ddb149149c6d096b869ad8aedada73
jbfp <jakob@jbfp.dk>
2021-01-18 10:29:04 +0100

    Rename chromecast::video::run

--- src/chromecast/video.rs
+++ src/chromecast/video.rs
@@ -7,16 +7,17 @@ use tokio::fs::File;
 #[get("/video/<path>")]
 pub(crate) async fn handler(path: &RawStr) -> Result<File, Debug<Error>> {
     let path = unescape(path);
-    Ok(run(&path).await?)
+    let file = load_file(&path).await?;
+    Ok(file)
 }
 
-async fn run(path: &str) -> Result<File, Error> {
+async fn load_file(path: &str) -> Result<File, Error> {
+    // todo: interpret the path as the full path?
+    trace!("path is {}", path);
     let mut root = HOME.clone();
     root.push(path);
     let root = root.canonicalize()?;
     let path = root.as_path();
-
     info!("loading video at {:#?}", path);
-
     Ok(File::open(path).await?)
 }
