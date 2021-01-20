cda5413c51fc52088010a8a35875b553f0b12069
jbfp <jakob@jbfp.dk>
2021-01-20 15:23:40 +0100

    Allow season, episode to be strings

--- src/subtitles/by_metadata.rs
+++ src/subtitles/by_metadata.rs
@@ -7,16 +7,16 @@ use rocket_contrib::json::Json;
 #[get("/subtitles/by-metadata?<title>&<season>&<episode>")]
 pub(crate) async fn handler(
     title: String,
-    season: Option<usize>,
-    episode: Option<usize>,
+    season: Option<String>,
+    episode: Option<String>,
 ) -> Result<Json<Vec<Subtitle>>, Debug<Error>> {
-    let url = format_url(title, season, episode);
+    let url = format_url(&title, season.as_deref(), episode.as_deref());
     let subtitles = opensubs::download_subtitles(&url).await?;
     info!("found {} subtitles", subtitles.len());
     Ok(Json(subtitles))
 }
 
-fn format_url(title: String, season: Option<usize>, episode: Option<usize>) -> String {
+fn format_url(title: &str, season: Option<&str>, episode: Option<&str>) -> String {
     let title = Uri::percent_encode(&title);
 
     let mut url = format!(
