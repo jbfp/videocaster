2ff8d76f70a6014ea5768b261fed8d98737f515b
jbfp <jakob@jbfp.dk>
2021-01-25 00:24:57 +0100

    Init chromecast in __onGCastApiAvailable

--- www/src/index.ts
+++ www/src/index.ts
@@ -6,10 +6,18 @@ import "./index.css";
 window.addEventListener("unload", server.shutdown);
 
 window.__onGCastApiAvailable = (isAvailable) => {
-    if (isAvailable) {
-        new App({
-            target: document.body,
-            props: {},
-        });
-    }
+    const context = cast.framework.CastContext.getInstance();
+
+    context.setOptions({
+        receiverApplicationId:
+            chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
+        autoJoinPolicy: chrome.cast.AutoJoinPolicy.TAB_AND_ORIGIN_SCOPED,
+    });
+
+    window["__isGCastApiAvailable"] = isAvailable;
 };
+
+new App({
+    target: document.body,
+    props: {},
+});
