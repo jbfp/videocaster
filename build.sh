adf86599aa42c4e1362af404da4c66aeef96226c
jbfp <jakob@jbfp.dk>
2021-01-25 21:52:10 +0100

    Build artifacts for .deb packages

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 rm -r www/public/build
-npm --prefix www run build
+rm -r dpkg/usr
 
-cargo clean -- release
+npm --prefix www run build
+cargo clean --release
 cargo build --release
+mkdir -p dpkg/usr/local/bin
+cp target/release/videocaster dpkg/usr/local/bin
+dpkg-deb --build dpkg
+mv dpkg.deb videocaster_1.0-.deb
