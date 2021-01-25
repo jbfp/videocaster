113c7401f6d3810f79a3ed465d7e399a71ca2a9f
jbfp <jakob@jbfp.dk>
2021-01-25 16:51:01 +0100

    Installer replaces .rc file

--- build.rs
+++
@@ -1,9 +0,0 @@
-extern crate embed_resource;
-
-fn main() {
-    if cfg!(target_os = "windows") {
-        println!("cargo:rerun-if-changed=app.ico");
-        println!("cargo:rerun-if-changed=app.rc");
-        embed_resource::compile("app.rc");
-    }
-}
